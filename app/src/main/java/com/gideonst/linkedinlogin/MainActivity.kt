3ad7b77d58a2fef71485eaa2ff58ce1e00c2c27a
gideon <dunprek@gmail.com>
2018-04-25 22:17:14 +0700

    update fix

--- app/src/main/java/com/gideonst/linkedinlogin/MainActivity.kt
+++ app/src/main/java/com/gideonst/linkedinlogin/MainActivity.kt
@@ -1,26 +1,27 @@
 package com.gideonst.linkedinlogin
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.linkedin.platform.AccessToken
 import com.linkedin.platform.LISessionManager
 import com.linkedin.platform.errors.LIAuthError
 import com.linkedin.platform.listeners.AuthListener
 import com.linkedin.platform.utils.Scope
-import com.linkedin.platform.utils.Scope.W_SHARE
-import com.linkedin.platform.utils.Scope.R_BASICPROFILE
 import com.linkedin.platform.APIHelper
 import com.linkedin.platform.errors.LIApiError
 import com.linkedin.platform.listeners.ApiResponse
 import com.linkedin.platform.listeners.ApiListener
+import android.util.Base64
+import org.json.JSONException
+import java.security.MessageDigest
+
+
 
 
 class MainActivity : AppCompatActivity() {
 
-
-    var url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name)"
 
     lateinit var apiHelper: APIHelper
 
@@ -30,35 +31,60 @@ class MainActivity : AppCompatActivity() {
 
         apiHelper = APIHelper.getInstance(applicationContext)
 
+        computePakageHash()
+
 
         LISessionManager.getInstance(applicationContext).init(this, buildScope(), object : AuthListener {
             override fun onAuthSuccess() {
                 // Authentication was successful.  You can now do
                 // other calls with the SDK.
+                getUsersData()
             }
 
             override fun onAuthError(error: LIAuthError) {
                 // Handle authentication errors
+                Log.e("ERORR", error.toString())
             }
         }, true)
 
 
+    }
+
+
+    private fun getUsersData() {
+     /*   val url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,public-profile-url,picture-url,email-address,picture-urls::(original))"*/
+        val url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address)"
+
         apiHelper.getRequest(this, url, object : ApiListener {
             override fun onApiSuccess(apiResponse: ApiResponse) {
                 // Success!
-
                 Log.d("MAIN ACTIVITY", apiResponse.toString())
+                try {
+                    val jsonObject = apiResponse.responseDataAsJson
+                    val firstName = jsonObject.getString("firstName")
+                    val lastName = jsonObject.getString("lastName")
+                    val emailAddress = jsonObject.getString("emailAddress")
+
+                    Log.d("TEST",firstName)
+
+
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
             }
 
             override fun onApiError(liApiError: LIApiError) {
                 // Error making GET request!
+                Log.e("ERRORAPI", liApiError.toString())
             }
         })
     }
 
     // Build the list of member permissions our LinkedIn session requires
     private fun buildScope(): Scope {
-        return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE)
+        return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE, Scope.R_EMAILADDRESS);
     }
 
 
@@ -66,6 +92,22 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
+
+    }
+
+    private fun computePakageHash() {
+        try {
+            val info = packageManager.getPackageInfo(
+                    "com.gideonst.linkedinlogin",
+                    PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            }
+        } catch (e: Exception) {
+            Log.e("TAG", e.message)
+        }
 
     }
 }
